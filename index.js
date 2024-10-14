30a0149ebd9d29425f96e01ea7b679d484705e6f
Peter Fänge <pellefange@gmail.com>
2024-10-14 20:49:53 +0200

    first commit - finished excercises

---
+++ index.js
@@ -0,0 +1,28 @@
+ // Exercise 1
+
+console.log("Hello World");
+
+
+// Exercise 2
+
+var name = "Peter"
+var age = "36"
+var fav_color = "Blue"
+
+console.log("My name is " + name + ", I am " + age + " years old, and my favourite color is " + fav_color + ".");
+
+
+//Exercise 3
+
+let x = 12
+let y = 6
+
+addition = x + y
+subtraction = x - y
+multiplication = x * y
+division = x / y
+
+console.log("x + y = " + addition);
+console.log("x - y = " + subtraction);
+console.log("x * y = " + multiplication);
+console.log("x / y = " + division);
